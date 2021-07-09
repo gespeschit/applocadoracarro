2b96aba55f78a8a1fb89a9c251bbe8eaf755e0ec
Guilherme Lima Espeschit <gespeschit@outlook.com>
2021-07-09 10:49:03 -0300

    Implementação da migrate modelos.

--- database/migrations/2021_07_09_131741_create_modelos_table.php
+++ database/migrations/2021_07_09_131741_create_modelos_table.php
@@ -14,10 +14,21 @@ class CreateModelosTable extends Migration
     public function up()
     {
         Schema::create('modelos', function (Blueprint $table) {
-            $table->id();
+            $table->id()->comment('Código de identificação única do registro');
+            $table->unsignedBigInteger('marca_id')->comment('Chave estrangeira relacionada a marcas');
+            $table->string('nome', 30)->comment('Nome do modelo');
+            $table->string('imagem', 100)->comment('Imagem do modelo');
+            $table->integer('numero_portas')->comment('Número de portas');
+            $table->integer('lugares')->comment('Número de lugares');
+            $table->boolean('air_bag')->comment('Se possui airbag');
+            $table->boolean('abs')->comment('Se possui freio abs');
             $table->timestamps();
+
+            //foreign key (constraints)
+            $table->foreign('marca_id')->references('id')->on('marcas');
         });
     }
+
 
     /**
      * Reverse the migrations.
